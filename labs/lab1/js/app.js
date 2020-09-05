187a95b0762de41008cfe925ac8343182d90cd1d
armills1992 <70212292+armills1992@users.noreply.github.com>
2020-09-04 20:15:39 -0400

    index/lab1

---
+++ labs/lab1/js/app.js
@@ -0,0 +1,27 @@
+function setup() {
+    var bg = "#170000";
+
+    createCanvas(800,600);
+
+    background(bg);
+
+    fill(140,9,0);
+    rect(125,200,350,200);
+
+    fill(0, 3, 87);
+    ellipse(300,300,100,50);
+
+    fill(87, 83, 0);
+    ellipse(200,300,100,50);
+    ellipse(400,300,100,50);
+    ellipse(300,250,50,60);
+    ellipse(300,350,50,60);
+
+    fill(77, 0, 87);
+    ellipse(125,300,50,200);
+    ellipse(475,300,50,200);
+
+    fill(0, 87, 1);
+    ellipse(300,195,350,50);
+    ellipse(300,405,350,50);
+}
